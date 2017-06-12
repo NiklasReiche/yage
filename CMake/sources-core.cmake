248238dba8804806075906c218c3e028053179f4
Niklas Reiche <reicheniklas@gmail.com>
2017-06-12 22:00:27 +0200

    Move Cmake sources to seperate file and update CMakeLists

---
+++ CMake/sources-core.cmake
@@ -0,0 +1,97 @@
+#########################
+# source/core/Cmake
+#########################
+
+set( YAGE_MATH_SOURCE
+
+	"${YAGE_CORE_PATH}/math/MVector.h"
+	
+)
+
+set( YAGE_GRAPHICS_SOURCE
+
+	"${YAGE_CORE_PATH}/graphics/gl.h"
+	"${YAGE_CORE_PATH}/graphics/Color.h"
+	"${YAGE_CORE_PATH}/graphics/Renderer.h"
+	"${YAGE_CORE_PATH}/graphics/ShaderLoader.h"
+	"${YAGE_CORE_PATH}/graphics/Graphics.h"
+
+)
+set( YAGE_GRAPHICS_GL3_SOURCE
+
+	"${YAGE_CORE_PATH}/graphics/GL3/gl3.h"
+	"${YAGE_CORE_PATH}/graphics/GL3/GL3_Exception.h"
+	"${YAGE_CORE_PATH}/graphics/GL3/GL3_Enum.h"
+	"${YAGE_CORE_PATH}/graphics/GL3/GL3_Object.h"
+	
+	"${YAGE_CORE_PATH}/graphics/GL3/GL3_Drawable.h"
+	"${YAGE_CORE_PATH}/graphics/GL3/GL3_Drawable.cpp"
+
+	"${YAGE_CORE_PATH}/graphics/GL3/GL3_Framebuffer.h"
+
+	"${YAGE_CORE_PATH}/graphics/GL3/GL3_Shader.h"
+	"${YAGE_CORE_PATH}/graphics/GL3/GL3_Shader.cpp"
+
+	"${YAGE_CORE_PATH}/graphics/GL3/GL3_Texture.h"
+	"${YAGE_CORE_PATH}/graphics/GL3/GL3_Texture.cpp"
+
+	"${YAGE_CORE_PATH}/graphics/GL3/GL3_Viewport.h"
+
+	"${YAGE_CORE_PATH}/graphics/GL3/GL3_UnitShader.h"
+	"${YAGE_CORE_PATH}/graphics/GL3/GL3_State.h"	
+	"${YAGE_CORE_PATH}/graphics/GL3/GL3_Context.h"
+	"${YAGE_CORE_PATH}/graphics/GL3/GL3_Context.cpp"
+
+)
+set( YAGE_GRAPHICS_GLES2_SOURCE
+
+	"${YAGE_CORE_PATH}/graphics/GLES2/gles2.h"
+	
+	"${YAGE_CORE_PATH}/graphics/GLES2/GLES2_Drawable.h"
+
+	"${YAGE_CORE_PATH}/graphics/GLES2/GLES2_Framebuffer.h"
+
+	"${YAGE_CORE_PATH}/graphics/GLES2/GLES2_Shader.h"
+
+	"${YAGE_CORE_PATH}/graphics/GLES2/GLES2_Texture.h"
+
+	"${YAGE_CORE_PATH}/graphics/GLES2/GLES2_Viewport.h"
+
+	"${YAGE_CORE_PATH}/graphics/GLES2/GLES2_Context.h"
+	"${YAGE_CORE_PATH}/graphics/GLES2/GLES2_Context.cpp"
+
+)
+
+set( YAGE_INPUT_SOURCE
+
+	"${YAGE_CORE_PATH}/input/InputNames.h"
+	"${YAGE_CORE_PATH}/input/InputState.h"
+	"${YAGE_CORE_PATH}/input/InputController.h"
+	"${YAGE_CORE_PATH}/input/InputController.cpp"
+	"${YAGE_CORE_PATH}/input/InputListener.h"
+	"${YAGE_CORE_PATH}/input/InputListener.cpp"
+
+)
+
+set( YAGE_PLATFORM_SOURCE
+
+	"${YAGE_CORE_PATH}/platform/Platform.h"
+	"${YAGE_CORE_PATH}/platform/GenericHandle.h"
+
+)
+set( YAGE_PLATFORM_DESKTOP_SOURCE
+
+	"${YAGE_CORE_PATH}/platform/desktop/GLFW_Callback.h"
+	"${YAGE_CORE_PATH}/platform/desktop/GLFW_Callback.cpp"
+	"${YAGE_CORE_PATH}/platform/desktop/GLFW_Handle.h"
+	"${YAGE_CORE_PATH}/platform/desktop/GLFW_Handle.cpp"
+
+)
+set( YAGE_PLATFORM_ANDROID_SOURCE
+
+	"${YAGE_CORE_PATH}/platform/android/Android_Handle.h"
+	"${YAGE_CORE_PATH}/platform/android/Android_Handle.cpp"
+	"${YAGE_CORE_PATH}/platform/android/android_native_app_glue.h"
+	"${YAGE_CORE_PATH}/platform/android/android_native_app_glue.c"
+
+)
