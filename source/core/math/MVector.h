6871d3ea22951263b2d081b4ce1971ab5ac4453f
Niklas Reiche <reicheniklas@gmail.com>
2017-07-16 21:49:09 +0200

    Add typedefs for short notation

--- source/core/math/MVector.h
+++ source/core/math/MVector.h
@@ -31,6 +31,23 @@ namespace gml
 	using Vec4 = Vector4D<T>;
 
 
+	typedef Vec2<int> Vec2i;
+	typedef Vec3<int> Vec3i;
+	typedef Vec4<int> Vec4i;
+
+	typedef Vec2<unsigned int> Vec2ui;
+	typedef Vec3<unsigned int> Vec3ui;
+	typedef Vec4<unsigned int> Vec4ui;
+
+	typedef Vec2<float> Vec2f;
+	typedef Vec3<float> Vec3f;
+	typedef Vec4<float> Vec4f;
+
+	typedef Vec2<double> Vec2d;
+	typedef Vec3<double> Vec3d;
+	typedef Vec4<double> Vec4d;
+
+
 	/*******************************
 	Classes
 	*******************************/
