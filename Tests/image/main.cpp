9e77bee2ab9a4f27ced17bd9f91472fd40723d72
Niklas Reiche <reicheniklas@gmail.com>
2017-09-09 15:16:07 +0200

    Update test

--- Tests/image/main.cpp
+++ Tests/image/main.cpp
@@ -8,16 +8,17 @@ int main()
 	img::ImageReader reader(&platform);
 	img::ImageWriter writer(&platform);
 
-	img::Image bitmap_1 = reader.readFile("../../../Tests/image/example_1.bmp");
-
-	std::cout << bitmap_1 << std::endl;
+	std::cout << "Start" << std::endl;
+	img::Image bitmap_1 = reader.readFile("../../../Tests/image/back.bmp");
+	std::cout << "End" << std::endl;
 
 	writer.writeFile(bitmap_1, "../../../Tests/image/example_2.bmp");
 
 
-	img::Image bitmap_2 = reader.readFile("../../../Tests/image/example_2.bmp");
+	//img::Image bitmap_2 = reader.readFile("../../../Tests/image/example_2.bmp");
 
-	std::cout << bitmap_2 << std::endl;
+	//std::cout << bitmap_2 << std::endl;
+	std::cout << "End" << std::endl;
 
 	std::cin.get();
 }
