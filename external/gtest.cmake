529f30c3107d0487cc9a30ae98de54ad647c4d22
Niklas Reiche <reicheniklas@gmail.com>
2017-10-07 22:49:30 +0200

    Add googletest library

--- external/gtest.cmake
+++ external/gtest.cmake
@@ -1,3 +1,9 @@
 ###################################################################
 # GTEST	                                                          #
 ###################################################################
+
+option(BUILD_GTEST "Builds the googletest subproject" ON)
+#Note that googlemock target already builds googletest
+option(BUILD_GMOCK "Builds the googlemock subproject" OFF)
+
+add_subdirectory( ${YAGE_EXTERNAL_PATH}/googletest )
