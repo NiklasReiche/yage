215f1c6ba24acdbbf3af18bc12724d159a2395ee
Niklas Reiche <reicheniklas@gmail.com>
2017-10-14 21:53:38 +0200

    Implement basic free camera model

--- source/modules/graphics3d/camera.cpp
+++ source/modules/graphics3d/camera.cpp
@@ -6,10 +6,58 @@ namespace graphics3d
 		this->position = gml::Vec3f(0.0f);
 		this->rotation = gml::Quaternion<float>();
 	}
-
 	Camera::Camera(gml::Vec3f position, gml::Quaternion<float> rotation) {
 		this->position = position;
 		this->rotation = rotation;
+	}
+
+	void Camera::move(gml::Vec3f vector)
+	{
+		this->position += vector;
+	}
+	void Camera::moveTo(gml::Vec3f position)
+	{
+		this->position = position;
+	}
+	void Camera::moveForward(float amount)
+	{
+		this->position += rotation.getForward() * amount;
+	}
+	void Camera::moveBackward(float amount)
+	{
+		this->position += rotation.getForward() * -amount;
+	}
+	void Camera::moveLeft(float amount)
+	{
+		this->position += rotation.getRight() * -amount;
+	}
+	void Camera::moveRight(float amount)
+	{
+		this->position += rotation.getRight() * amount;
+	}
+
+	void Camera::rotate(gml::Quaternion<float> quaternion)
+	{
+		this->rotation *= quaternion;
+	}
+	void Camera::rotateTo(gml::Quaternion<float> rotation)
+	{
+		this->rotation = rotation;
+	}
+	void Camera::rotateYaw(float degree)
+	{
+		this->rotation = this->rotation * gml::eulerAngle<float>(0, degree, 0);
+		this->rotation = gml::normalize<float>(this->rotation);
+	}
+	void Camera::rotatePitch(float degree)
+	{
+		this->rotation = this->rotation * gml::eulerAngle<float>(degree, 0, 0);
+		this->rotation = gml::normalize<float>(this->rotation);
+	}
+	void Camera::rotateRoll(float degree)
+	{
+		this->rotation = this->rotation * gml::eulerAngle<float>(0, 0, degree);
+		this->rotation = gml::normalize<float>(this->rotation);
 	}
 
 	gml::Matrix4D<float> Camera::getViewMatrix() {
--- source/modules/graphics3d/camera.h
+++ source/modules/graphics3d/camera.h
@@ -2,21 +2,31 @@
 
 #include <math/MVector.h>
 
-
 namespace graphics3d
 {
 	class Camera
 	{
 	private:
-		
+		gml::Vec3f position;
+		gml::Quaternion<float> rotation;
 
 	public:
 		Camera();
 		Camera(gml::Vec3f position, gml::Quaternion<float> rotation);
-		void update();
-		gml::Matrix4D<float> getViewMatrix();
+		
+		void move(gml::Vec3f move);
+		void moveTo(gml::Vec3f position);
+		void moveForward(float amount);
+		void moveBackward(float amount);
+		void moveLeft(float amount);
+		void moveRight(float amount);
 
-		gml::Vec3f position;
-		gml::Quaternion<float> rotation;
+		void rotate(gml::Quaternion<float> rotate);
+		void rotateTo(gml::Quaternion<float> rotation);
+		void rotateYaw(float degree);
+		void rotatePitch(float degree);
+		void rotateRoll(float degree);
+
+		gml::Matrix4D<float> getViewMatrix();
 	};
 }
