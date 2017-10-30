711681e47421f2222bf22d96c021f78708a283f0
Niklas Reiche <reicheniklas@gmail.com>
2017-10-30 22:05:43 +0100

    Fix gTest linking with MT

--- external/gtest.cmake
+++ external/gtest.cmake
@@ -6,4 +6,9 @@ option(BUILD_GTEST "Builds the googletest subproject" ON)
 #Note that googlemock target already builds googletest
 option(BUILD_GMOCK "Builds the googlemock subproject" OFF)
 
+option(
+  gtest_force_shared_crt
+  "Use shared (DLL) run-time lib even when Google Test is built as static lib."
+  ON)
+
 add_subdirectory( ${YAGE_EXTERNAL_PATH}/googletest )
