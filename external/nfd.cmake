b4eeb370c39a74619a9260d99cb934cfa317ed1f
Niklas Reiche <29310846+NiklasReiche@users.noreply.github.com>
2017-11-11 21:27:01 +0100

    Remove nfd

--- external/nfd.cmake
+++
@@ -1,6 +0,0 @@
-###################################################################
-# NATIVE FILE DIALOG	                                          #
-###################################################################
-
-add_subdirectory(${YAGE_EXTERNAL_PATH}/nfd)
-target_include_directories(nfd PUBLIC $<BUILD_INTERFACE:${YAGE_EXTERNAL_PATH}/nfd/include>)
