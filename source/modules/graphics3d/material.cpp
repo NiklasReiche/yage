3f6107a757ade246ff41a0b9c4985bcb82967def
Niklas Reiche <29310846+NiklasReiche@users.noreply.github.com>
2018-02-17 20:23:17 +0100

    Add material

--- source/modules/graphics3d/material.cpp
+++ source/modules/graphics3d/material.cpp
@@ -1,6 +1,54 @@
 #include "material.h"
 
-namespace graphics3d
+namespace gl3d
 {
+	void Material::addTexture( std::string name, gl::Texture tex)
+	{
+		textures[name] = tex;
+	}
 
+	void Material::addVec3(std::string name, gml::Vec3f value)
+	{
+		vec3Values[name] = value;
+	}
+
+	void Material::addFloat(std::string name, float value)
+	{
+		fValues[name] = value;
+	}
+
+	void Material::addInteger(std::string name, int value)
+	{
+		iValues[name] = value;
+	}
+
+	void Material::updateShader()
+	{
+		for (std::pair<std::string, gml::Vec3f> uniform : vec3Values)
+		{
+			shader.setUniform("material." + uniform.first, uniform.second);
+		}
+		for (std::pair<std::string, float> uniform : fValues)
+		{
+			shader.setUniform("material." + uniform.first, uniform.second);
+		}
+		for (std::pair<std::string, int> uniform : iValues)
+		{
+			shader.setUniform("material." + uniform.first, uniform.second);
+		}
+		for (std::pair<std::string, gl::Texture> uniform : textures)
+		{
+			//TODO 
+		}
+	}
+
+	void Material::setShader(gl::Shader shader)
+	{
+		this->shader = shader;
+	}
+
+	gl::Shader Material::getShader() const
+	{
+		return shader;
+	}
 }
