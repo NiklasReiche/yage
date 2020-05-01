1c207f18e5af9b9b0aad2fc26e8f804343e99be7
Niklas Reiche <29310846+NiklasReiche@users.noreply.github.com>
2020-05-01 22:10:37 +0200

    Update build config for gcc

--- external/gtest.cmake
+++ external/gtest.cmake
@@ -12,3 +12,16 @@ option(
   ON)
 
 add_subdirectory( ${YAGE_EXTERNAL_PATH}/googletest )
+
+target_compile_options(gtest
+        PRIVATE
+        $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
+        -w>
+        $<$<CXX_COMPILER_ID:MSVC>:
+        >)
+target_compile_options(gmock
+        PRIVATE
+        $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
+        -w>
+        $<$<CXX_COMPILER_ID:MSVC>:
+        >)
