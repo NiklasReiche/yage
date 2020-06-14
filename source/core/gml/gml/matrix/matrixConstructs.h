001f46dd6bd14fd37d33531ae9ef981015b79ceb
Niklas Reiche <29310846+NiklasReiche@users.noreply.github.com>
2020-06-14 18:10:56 +0200

    Split matrix files

--- source/core/gml/gml/matrix/matrixConstructs.h
+++ source/core/gml/gml/matrix/matrixConstructs.h
@@ -1,41 +1,79 @@
 #pragma once
 
+#include "mat4.h"
+
 namespace gml
 {
+	/**
+	 * @brief Constructs a translation matrix.
+	 *
+	 * @param x the x component
+	 * @param y the y component
+	 * @param z the z component
+	 * @return the translation matrix
+	*/
 	template<typename T>
-	Mat4 <T> Mat4<T>::translate(const T& x, const T& y, const T& z)
+	Mat4<T> translate(const T& x, const T& y, const T& z)
 	{
-		Mat4<T> result = Id;
+		Mat4<T> result = Mat4<T>::Id;
 		result(0, 3) = x;
 		result(1, 3) = y;
 		result(2, 3) = z;
 		return result;
 	}
 
+	/**
+	 * @brief Constructs a translation matrix from a vector.
+	 *
+	 * @param translation the translation vector
+	 * @return the translation matrix
+	 */
 	template<typename T>
-	Mat4 <T> Mat4<T>::translate(const Vec3 <T>& translation)
+	Mat4<T> translate(const Vec3<T>& translation)
 	{
 		return Mat4<T>::translate(translation(0), translation(1), translation(2));
 	}
 
+	/**
+	 * @brief Constructs a scaling matrix.
+	 *
+	 * @param x the x component
+	 * @param y the y component
+	 * @param z the z component
+	 * @return the scaling matrix
+	 */
 	template<typename T>
-	Mat4 <T> Mat4<T>::scale(const T& x, const T& y, const T& z)
+	Mat4<T> scale(const T& x, const T& y, const T& z)
 	{
-		Mat4<T> result = Id;
+		Mat4<T> result = Mat4<T>::Id;
 		result(0, 0) = x;
 		result(1, 1) = y;
 		result(2, 2) = z;
 		return result;
 	}
 
+	/**
+	 * @brief Constructs a scaling matrix from a vector.
+	 *
+	 * @param scale the scaling vector
+	 * @return the scaling matrix
+	 */
 	template<typename T>
-	Mat4 <T> Mat4<T>::scale(const Vec3 <T>& scale)
+	Mat4<T> scale(const Vec3<T>& scale)
 	{
 		return Mat4<T>::scale(scale.at(0), scale.at(1), scale.at(2));
 	}
 
+	/**
+	 * @brief Constructs a rotation matrix from an axis angle
+	 * representation.
+	 *
+	 * @param axis the rotation axis
+	 * @param angle the angle in radians
+	 * @return the rotation matrix
+	 */
 	template<typename T>
-	Mat4 <T> Mat4<T>::axisAngle(const Vec3 <T>& axis, const double angle)
+	Mat4<T> axisAngle(const Vec3<T>& axis, const double angle)
 	{
 		Vec3<T> a = gml::normalize(axis);
 		const T x = a.x;
@@ -60,8 +98,14 @@ namespace gml
 		return result;
 	}
 
+	/**
+	 * @brief Constructs a rotation matrix from a quaternion
+	 * representation.
+	 *
+	 * @return the rotation matrix
+	 */
 	template<typename T>
-	Mat4 <T> Mat4<T>::quaternion(const Quaternion <T>& quaternion)
+	Mat4<T> quaternion(const Quaternion<T>& quaternion)
 	{
 		const Quaternion<T> q = normalize(quaternion);;
 
@@ -80,10 +124,17 @@ namespace gml
 		return result;
 	}
 
+	/**
+	 * @brief Constructs a perspective projection matrix.
+	 *
+	 * @param fov the field of view in degrees
+	 * @param aspect the aspect ration
+	 * @param near the near plane
+	 * @param far the far plane
+	 * @return the projection matrix
+	 */
 	template<typename T>
-	Mat4 <T> Mat4<T>::perspective(
-		const double fov, const double aspect,
-		const double near, const double far)
+	Mat4<T> perspective(const double fov, const double aspect, const double near, const double far)
 	{
 		Mat4<T> result;
 		const double top = near * std::tan(toRad(fov / 2));
@@ -101,8 +152,19 @@ namespace gml
 		return result;
 	}
 
+	/**
+	 * @brief Constructs an orthographic projection matrix.
+	 *
+	 * @param left the left plane
+	 * @param right the right plane
+	 * @param bottom the bottom plane
+	 * @param top the top plane
+	 * @param near the near plane
+	 * @param far the far plane
+	 * @return the projection matrix
+	 */
 	template<typename T>
-	Mat4 <T> Mat4<T>::orthographic(
+	Mat4<T> orthographic(
 		const double left, const double right,
 		const double bottom, const double top,
 		const double near, const double far)
@@ -118,11 +180,16 @@ namespace gml
 		return result;
 	}
 
+	/**
+	 * @brief Constructs a lookAt view matrix.
+	 *
+	 * @param pos the camera position
+	 * @param target the view target position
+	 * @param up the camera up vector
+	 * @return the view matrix
+	 */
 	template<typename T>
-	Mat4 <T> Mat4<T>::lookAt(
-		const Vec3 <T>& pos,
-		const Vec3 <T>& target,
-		const Vec3 <T>& up)
+	Mat4<T> lookAt(const Vec3<T>& pos, const Vec3<T>& target, const Vec3<T>& up)
 	{
 		Vec3<T> direction = normalize(pos - target);
 		Vec3<T> right = normalize(cross(up, direction));
