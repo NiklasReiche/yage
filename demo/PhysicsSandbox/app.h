33e78dad35cbe4d53a32936af57a0768c5e55d5f
Niklas Reiche <29310846+NiklasReiche@users.noreply.github.com>
2024-05-22 12:38:56 +0200

    Update demo

--- demo/PhysicsSandbox/app.h
+++ demo/PhysicsSandbox/app.h
@@ -10,10 +10,12 @@
 #include "gl3d/sceneRenderer.h"
 #include "gl3d/resources/obj.h"
 #include "gl3d/resources/gltf.h"
+#include "gl3d/light.h"
 #include "physics3d/Simulation.h"
 
 #include "MovementListener.h"
 #include "ProjectionView.h"
+#include "gl3d/shaders.h"
 
 class App
 {
@@ -34,18 +36,7 @@ public:
 		baseRenderer->setClearColor(0x008080FFu);
 		baseRenderer->enableDepthTest();
 		baseRenderer->setViewport(0, 0, 1500, 900);
-		renderer = gl3d::SceneRenderer(baseRenderer);
-
-
-		std::string vertexShader =
-#include "gl3d/shaders/singleColorShader.vert"
-	;
-		std::string fragmentShader =
-#include "gl3d/shaders/singleColorShader.frag"
-	;
-
-		shader = glContext->getShaderCreator()->createShader(vertexShader, fragmentShader);
-
+		renderer = std::make_shared<gl3d::SceneRenderer>(baseRenderer);
 
 		auto fileReader = platform::desktop::FileReader();
 		std::string csVertexShader = fileReader.
@@ -56,13 +47,8 @@ public:
 			openTextFile("shaders/pointShader.geom", platform::IFile::AccessMode::READ)->readAll();
 		csShader = glContext->getShaderCreator()->createShader(csVertexShader, csFragmentShader, csGeometryShader);
 
-        std::string pbrVertexShader =
-#include "gl3d/shaders/pbrShader.vert"
-        ;
-        std::string pbrFragmentShader =
-#include "gl3d/shaders/pbrShader.frag"
-        ;
-		pbrShader = glContext->getShaderCreator()->createShader(pbrVertexShader, pbrFragmentShader);
+        pbrShader = glContext->getShaderCreator()->createShader(gl3d::shaders::PbrShader::vert,
+                                                                gl3d::shaders::PbrShader::frag);
 
 		auto ubo = glContext->getShaderCreator()->createUniformBlock("ProjectionView");
 		pbrShader->linkUniformBlock(*ubo);
@@ -72,6 +58,7 @@ public:
 		projViewUniform.projection = gml::matrix::perspective<float>(45.0f, 1500.0f / 900.0f, 0.1f, 1000.0f);
 		projViewUniform.syncProjection();
 
+        scene = std::make_shared<gl3d::SceneGroup>("world");
 		loadResources();
 	}
 
@@ -81,20 +68,15 @@ public:
 		window->show();
 		std::static_pointer_cast<platform::desktop::GlfwWindow>(window)->getTimeStep();
 
-		auto cube1 = loadModel("models/sphere.gltf",
+		auto ball1 = loadModel("models/baseball/scene.gltf",
                                physics3d::SphereShape(1, 1),
                                gml::Vec3d(0, 0, 0));
-		cube1->applyForce(gml::Vec3d(0, 20, 0), gml::Vec3d(0.3, -0.5, 0));
+        ball1->applyForce(gml::Vec3d(0, 20, 0), gml::Vec3d(0.3, -0.5, 0));
 
-		auto cube2 = loadModel("models/cube.gltf",
-                               physics3d::CubeShape(1, 100),
-                               gml::Vec3d(-5, 0, 0));
-		cube2->applyForce(gml::Vec3d(-30, 1, 10), gml::Vec3d(0.5, 0, 0.5));
-
-		auto cube3 = loadModel("models/cube.gltf",
-                               physics3d::CubeShape(1, 10),
+        auto ball2 = loadModel("models/baseball/scene.gltf",
+                               physics3d::SphereShape(1, 1),
                                gml::Vec3d(5, 0, 0));
-		cube3->applyForce(gml::Vec3d(3, -1, 0), gml::Vec3d(0, -0.5, 0));
+        ball2->applyForce(gml::Vec3d(-5, 0, 0), gml::Vec3d(0, 0.1, 0));
 
 		auto point = glContext->getDrawableCreator()->createDrawable(std::vector<float>{ },
 		                                                             std::vector<unsigned int>{ },
@@ -114,16 +96,12 @@ public:
 			projViewUniform.syncView();
 
 			baseRenderer->useShader(*csShader);
-			csShader->setUniform("viewPos", camera->getPosition());
 			baseRenderer->draw(*point);
 
 			baseRenderer->useShader(*pbrShader);
 			pbrShader->setUniform("camPos", camera->getPosition());
-			pbrShader->setUniform("n_pointLights", 1);
-			pbrShader->setUniform("pointLights[0].position", gml::Vec3f(0, 1, 5));
-			pbrShader->setUniform("pointLights[0].color", gml::Vec3f(50, 50, 100));
 
-			renderer.renderGraph(scene);
+			renderer->renderGraph(scene);
 
 			window->swapBuffers();
 			window->pollEvents();
@@ -142,13 +120,12 @@ private:
 	std::shared_ptr<gl::IShader> pbrShader;
 	std::shared_ptr<gl3d::Camera> camera;
 	std::shared_ptr<gl::IRenderer> baseRenderer;
-	gl3d::SceneRenderer renderer;
+	std::shared_ptr<gl3d::SceneRenderer> renderer;
 
 	std::shared_ptr<gl3d::SceneGroup> scene;
-	std::shared_ptr<gl3d::SceneObject> light1;
 
 	physics3d::Simulation simulation;
-	std::vector<std::tuple<std::shared_ptr<gl3d::SceneObject>, std::shared_ptr<physics3d::RigidBody>>> objects;
+	std::vector<std::tuple<std::shared_ptr<gl3d::SceneNode>, std::shared_ptr<physics3d::RigidBody>>> objects;
 
 	void updateSceneGraph()
 	{
@@ -162,57 +139,34 @@ private:
 
 	std::shared_ptr<physics3d::RigidBody> loadModel(const std::string& filename, physics3d::InertiaShape shape, gml::Vec3d position)
 	{
-		auto tuple = gl3d::resources::readGltf(platform::desktop::FileReader(),
-		                                       filename, *glContext->getDrawableCreator());
-		auto mesh = std::make_shared<gl3d::Mesh>(std::move(std::get<0>(tuple)));
-		auto material = std::make_shared<gl3d::Material>(std::get<1>(tuple));
-        material->set_shader(pbrShader);
-
-		auto object = std::make_shared<gl3d::SceneObject>(&""[std::rand()]);
-		object->bindMaterial(material);
-		object->bindMesh(mesh);
-		scene->addChild(object);
+        auto model = gl3d::resources::readGltf(platform::desktop::FileReader(),
+                                               filename, *glContext->getDrawableCreator(),
+                                               *glContext->getTextureCreator(), pbrShader);
+		scene->addChild(model);
 
 		auto rb = std::make_shared<physics3d::RigidBody>(shape, position, gml::Quatd());
 		simulation.addRigidBody(rb);
 
-		objects.emplace_back(object, rb);
+		objects.emplace_back(model, rb);
 		return rb;
 	}
 
 	void loadResources()
 	{
-		scene = std::make_shared<gl3d::SceneGroup>("world");
+		auto lightRes = std::make_shared<gl3d::DirectionalLight>();
+        lightRes->color = gml::Vec3f(4);
+		auto light = std::make_shared<gl3d::SceneObject>("light");
+        light->bindLight(lightRes);
+        light->setTransform(
+			gml::matrix::fromQuaternion<double>(gml::quaternion::eulerAngle<double>(3.14, 0, 0)));
+		scene->addChild(light);
 
-		auto lightRes = std::make_shared<gl3d::DirectionalLight>(
-			gl3d::DirectionalLight({gml::Vec3f(0.2f),
-                                    gml::Vec3f(0.5f),
-                                    gml::Vec3f(1.0f) },
-                                   gml::Vec3f(-1, -1, -1)));
-		light1 = std::make_shared<gl3d::SceneObject>("light");
-		light1->bindLight(lightRes);
-		light1->setTransform(
-			gml::matrix::fromQuaternion<double>(gml::quaternion::eulerAngle<double>(0, 0, 0)) *
-			gml::matrix::translate<double>(gml::Vec3d(0.0, 0.0, -3.0)) *
-			gml::matrix::scale<double>(0.1f, 0.1f, 0.1f));
-
-		auto lightRes2 = std::make_shared<gl3d::PointLight>(
-			gl3d::PointLight(
-				{ gml::Vec3f(1), gml::Vec3f(1), gml::Vec3f(1) },
-				{ 1.0f, 0.09f, 0.032f }));
-		auto light2 = std::make_shared<gl3d::SceneObject>("light2");
-		light2->bindLight(lightRes2);
-
-		auto tuple = gl3d::resources::readGltf(platform::desktop::FileReader(),
-		                                       "models/cube.gltf", *glContext->getDrawableCreator());
-		auto mesh = std::make_shared<gl3d::Mesh>(std::move(std::get<0>(tuple)));
-		auto material = std::make_shared<gl3d::Material>(std::get<1>(tuple));
-        material->set_shader(pbrShader);
-		light2->bindMesh(mesh);
-		light2->bindMaterial(material);
-		light2->setTransform(gml::matrix::translate<double>(gml::Vec3d(0, 1, 5)) *
-			                     gml::matrix::scale<double>(0.1, 0.1, 0.1));
-
-		scene->addChild(light2);
+        lightRes = std::make_shared<gl3d::DirectionalLight>();
+        lightRes->color = gml::Vec3f(2.5);
+        light = std::make_shared<gl3d::SceneObject>("light");
+        light->bindLight(lightRes);
+        light->setTransform(
+                gml::matrix::fromQuaternion<double>(gml::quaternion::eulerAngle<double>(0, 0, 0)));
+        scene->addChild(light);
 	}
 };
