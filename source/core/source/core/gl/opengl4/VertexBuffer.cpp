ff5819a0e23ff3fd1eaa99a6e32e7aac233510c3
Niklas Reiche <29310846+NiklasReiche@users.noreply.github.com>
2024-07-18 00:09:27 +0200

    Implement vertex buffers

--- source/core/source/core/gl/opengl4/VertexBuffer.cpp
+++ source/core/source/core/gl/opengl4/VertexBuffer.cpp
@@ -1,5 +1,53 @@
 #include "VertexBuffer.h"
 
+#include "Context.h"
+#include "enums.h"
+
 namespace yage::gl::opengl4
 {
+    VertexBuffer::VertexBuffer(Context* context, const std::span<const std::byte> data, const ResourceUsage usage)
+        : m_context(context)
+    {
+        glGenBuffers(1, &m_vbo_handle);
+
+        m_context->bind_buffer(GL_ARRAY_BUFFER, m_vbo_handle);
+        glBufferData(GL_ARRAY_BUFFER, static_cast<GLsizeiptr>(data.size()), data.data(), convert(usage));
+    }
+
+    VertexBuffer::~VertexBuffer()
+    {
+        clear();
+    }
+
+    VertexBuffer::VertexBuffer(VertexBuffer&& other) noexcept
+        : m_context(other.m_context),
+          m_vbo_handle(other.m_vbo_handle)
+    {
+        other.m_context = nullptr;
+        other.m_vbo_handle = 0;
+    }
+
+    VertexBuffer& VertexBuffer::operator=(VertexBuffer&& other) noexcept
+    {
+        if (this == &other)
+            return *this;
+
+        clear();
+
+        m_context = other.m_context;
+        m_vbo_handle = other.m_vbo_handle;
+
+        other.m_context = nullptr;
+        other.m_vbo_handle = 0;
+
+        return *this;
+    }
+
+    void VertexBuffer::clear()
+    {
+        if (m_context != nullptr) {
+            glDeleteBuffers(1, &m_vbo_handle);
+            m_context->unbind_buffer(GL_ARRAY_BUFFER, m_vbo_handle);
+        }
+    }
 }
--- source/core/source/core/gl/opengl4/VertexBuffer.h
+++ source/core/source/core/gl/opengl4/VertexBuffer.h
@@ -1,8 +1,36 @@
 #pragma once
 
+#include <span>
+
+#include <glad/gl.h>
+
+#include "../IVertexBuffer.h"
+#include "../enums.h"
+
 namespace yage::gl::opengl4
 {
-    class VertexBuffer
+    class Context;
+
+    class VertexBuffer final : public IVertexBuffer
     {
+    public:
+        VertexBuffer(Context* context, std::span<const std::byte> data, ResourceUsage usage);
+
+        ~VertexBuffer() override;
+
+        VertexBuffer(const VertexBuffer& other) = delete;
+
+        VertexBuffer(VertexBuffer&& other) noexcept;
+
+        VertexBuffer& operator=(const VertexBuffer& other) = delete;
+
+        VertexBuffer& operator=(VertexBuffer&& other) noexcept;
+
+    private:
+        Context* m_context;
+
+        GLuint m_vbo_handle = 0;
+
+        void clear();
     };
 }
